abca8be3d4801cbccf0ebf9f1808b71916a04d78
eddi3v <ed@avioh.com>
2024-07-12 12:55:02 +0100

    New theme and shell

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,11 @@
 local wezterm = require 'wezterm'
+local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
 return {
-    default_prog = {'C:/cygwin64/Cygwin.bat'},
+    default_prog = {'nu'},
     --font = wezterm.font('GohuFont 14 Nerd Font Mono'),
     --font = wezterm.font('BigBlueTerm437 Nerd Font'),
-    font = wezterm.font('FiraCode NF'),
-    font_size = 11.0,
+    font = wezterm.font('FiraCode Nerd Font'),
+    font_size = 12.0,
     line_height = 1.0,
     keys = {
         {
@@ -19,12 +20,13 @@ return {
         },
     },
     window_padding = {
-        left = 0,
-        right = 0,
-        top = 0,
-        bottom = 0,
+        left = 10,
+        right = 10,
+        top = 10,
+        bottom = 10,
     },
-    color_scheme = 'Papercolor Dark (Gogh)',
+    colors = theme.colors(),
+    window_frame = theme.window_frame(),
     hide_tab_bar_if_only_one_tab = true,
     use_fancy_tab_bar = false,
     window_close_confirmation = 'NeverPrompt'
