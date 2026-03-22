a0dd23883bdf0885a4597010c3b838448e7eab19
Eduard Vaduva <ed@avioh.com>
2026-03-22 20:26:56 +0000

    updated keybindings

--- wezterm.lua
+++ wezterm.lua
@@ -3,10 +3,27 @@ local config = wezterm.config_builder()
 
 config.default_prog = { 'nu' }
 
+config.max_fps = 240
+config.animation_fps = 240
+config.tab_max_width = 1600
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.hide_tab_bar_if_only_one_tab = false
+config.use_fancy_tab_bar = true
+config.window_close_confirmation = 'NeverPrompt'
+config.window_padding = {
+    left = 10,
+    right = 10,
+    top = 10,
+    bottom = 10,
+}
+
 -- font settings
 config.font = wezterm.font('Berkeley Mono')
 config.font_size = 12.0
 config.line_height = 1.0
+
+-- colors
+config.color_scheme = 'Moonfly (Gogh)'
 
 -- keybindings
 config.keys = {
@@ -17,24 +34,54 @@ config.keys = {
     },
     {
         key = 'w',
-        mods = 'SHIFT',
+        mods = 'CTRL',
         action = wezterm.action.CloseCurrentPane { confirm = false },
     },
+    {
+        key = 't',
+        mods = 'CTRL',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+    },
+    {
+		key = "s",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "h",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		key = "l",
+		mods = "CTRL",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		key = "l",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "j",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		key = "k",
+		mods = "CTRL|SHIFT",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+	},
 }
-
--- colors
-config.color_scheme = 'Moonfly (Gogh)'
-
--- window padding and tab bar settings
-config.window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
-}
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.hide_tab_bar_if_only_one_tab = false
-config.use_fancy_tab_bar = true
-config.window_close_confirmation = 'NeverPrompt'
 
 return config
